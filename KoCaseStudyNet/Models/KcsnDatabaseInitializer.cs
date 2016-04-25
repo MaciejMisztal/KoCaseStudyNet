e6d999ce4e27d973abf1a3785f0baa27e889dc72
MaciejMisztal <maciejmisztal@op.pl>
2016-04-25 02:23:46 +0200

    init

---
+++ KoCaseStudyNet/Models/KcsnDatabaseInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace KoCaseStudyNet.Models
+{
+    public class KcsnDatabaseInitializer : DropCreateDatabaseAlways<KcsnCtx>
+    {
+        protected override void Seed(KcsnCtx context)
+        {
+            SeedDatabase(context);
+        }
+        public static void SeedDatabase(KcsnCtx context)
+        {
+        }
+    }
+}
